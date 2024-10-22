ee2cdae7a2168b6ceaac9a621c44171d297ed7b6
Axel Olea <axeloleabarrera@gmail.com>
2024-10-21 23:18:24 -0300

    Avance 4

--- src/app/pages/cerrar-sesion/cerrar-sesion.component.ts
+++ src/app/pages/cerrar-sesion/cerrar-sesion.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cerrar-sesion',
@@ -10,9 +11,12 @@ export class CerrarSesionComponent  implements OnInit {
 
   private authService = inject(AuthService);  // Inyecta el servicio de autenticación
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate(['']); // Redirige al home después de 3 segundos
+    }, 3000); // 3000 milisegundos = 3 segundos
     this.authService.logout();  // Cierra la sesión
   }
 
